a51116ade1cecee2a8099503b340112bb41d5eaf
romankarki <romnkrki@gmail.com>
2020-09-04 14:00:30 -0400

    Updates

---
+++ precache-manifest.af27bb61ab06941facd9e2571cd41ef6.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6c6db66ba3deb9e9d302d442284355",
+    "url": "/myportfolio/index.html"
+  },
+  {
+    "revision": "83b1d8fbba4651bf9ba6",
+    "url": "/myportfolio/static/css/main.c92e072d.chunk.css"
+  },
+  {
+    "revision": "dbf0a48abe7ff34c5c17",
+    "url": "/myportfolio/static/js/2.f83095c4.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/myportfolio/static/js/2.f83095c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83b1d8fbba4651bf9ba6",
+    "url": "/myportfolio/static/js/main.80c209c2.chunk.js"
+  },
+  {
+    "revision": "da2e02db1e870a8bb8e5",
+    "url": "/myportfolio/static/js/runtime-main.b5f0a273.js"
+  },
+  {
+    "revision": "76a839b57ab7086b33a469e20d444fe5",
+    "url": "/myportfolio/static/media/burger.76a839b5.png"
+  },
+  {
+    "revision": "6a1cd62e98b908b48fb7f6942e8a010b",
+    "url": "/myportfolio/static/media/ecommerce.6a1cd62e.png"
+  },
+  {
+    "revision": "614f540c85f35b0ef96cfbaeb60fad00",
+    "url": "/myportfolio/static/media/expenses.614f540c.png"
+  },
+  {
+    "revision": "6b319e2f62a5b970336a371666594340",
+    "url": "/myportfolio/static/media/landing.6b319e2f.png"
+  },
+  {
+    "revision": "ad3163375b8effdedb52b055324b390c",
+    "url": "/myportfolio/static/media/library.ad316337.png"
+  },
+  {
+    "revision": "402e707f0076a0ebca94d19e49376832",
+    "url": "/myportfolio/static/media/monkey.402e707f.png"
+  },
+  {
+    "revision": "21cec34784aa501904bd711a6f75981d",
+    "url": "/myportfolio/static/media/pexels-pixabay-459225.21cec347.jpg"
+  },
+  {
+    "revision": "59a232d7055acc1aa558402eb2745f86",
+    "url": "/myportfolio/static/media/portfolio.59a232d7.pdf"
+  },
+  {
+    "revision": "cbc8b8ff2dd9d47f538b6858af1e33fe",
+    "url": "/myportfolio/static/media/stdmgmt.cbc8b8ff.png"
+  },
+  {
+    "revision": "2f226df99e910c85ace0b55035863597",
+    "url": "/myportfolio/static/media/todo.2f226df9.png"
+  }
+]);
